47edb507ae768828e4634dff4dc464a8e2ef0e66
steve ulrich <sulrich@botwerks.org>
2020-01-13 08:21:28 -0600

    added a function to cleanup the links with pyenv and brew install

--- zsh-custom/platform/Darwin.zsh
+++ zsh-custom/platform/Darwin.zsh
@@ -13,3 +13,18 @@ alias mutt="neomutt"
 function set_volume() { sudo osascript -e "set Volume $1" }
 function ql () { qlmanage -p "$@" >& /dev/null & }
 function iterm-title () { echo -ne "\033]0;"${1}"\007" }
+
+# sometimes we want to install something using the homebrew version of python.
+# this is expecially useful when dealing with things that happen to be linked
+# against the brew python installs and there's a pip module we need.
+pyenv-brew-relink() {
+  rm -f "${HOME}/.pyenv/versions/*-brew"
+
+  for i in $(brew --cellar python)/*; do
+    ln -s "${i}" "${HOME}/.pyenv/versions/${i##/*/}-brew"
+  done
+
+  for i in $(brew --cellar python@2)/*; do
+    ln -s "${i}" "${HOME}/.pyenv/versions/${i##/*/}-brew"
+  done
+}
