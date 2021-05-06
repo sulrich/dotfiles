21c53da5adb9551c8746e4fb085f4ce6a4d2b2e1
steve ulrich <sulrich@botwerks.org>
2021-05-06 12:02:46 -0500

    environment variable cleanup.

--- zsh-custom/hosts/abby-env.zsh
+++ zsh-custom/hosts/abby-env.zsh
@@ -1,8 +1,4 @@
 # -*- Mode: shell-script; tab-width: 2 -*- *
-# lanesboro-pi specific environment variables
-
-# referenced somewhere, i just don't quite know where ...
-# TODO: this needs to updated in the age of pyenv
-export POWERLINE_DIR="${HOME}/.pyenv/versions/3.7.4/lib/python3.7/site-packages/powerline"
+# abby.botwerks.net specific environment variables
 
 export HUGO_DIR="${HOME}/src/botwerks-site"
