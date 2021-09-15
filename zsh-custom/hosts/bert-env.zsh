45cab319c41d3c3fafda22dd98c0861fe20bf820
steve ulrich <sulrich@botwerks.org>
2021-09-15 07:38:31 -0500

    removed most of the bert env configs

--- zsh-custom/hosts/bert-env.zsh
+++ zsh-custom/hosts/bert-env.zsh
@@ -1,13 +1,6 @@
 # -*- Mode: shell-script; tab-width: 2 -*- *
 # bert specific environment variables
 
-# blog base directory 
-export HUGO_DIR="${HOME}/src/botwerks-site"
-
-
-# referenced somewhere, i just don't quite know where ...
-# TODO: this needs to updated in the age of pyenv
-export POWERLINE_DIR="${HOME}/.local/lib/python3.5/site-packages/powerline"
 
 # pyenv location
 export PYENV_ROOT="${HOME}/.pyenv"
