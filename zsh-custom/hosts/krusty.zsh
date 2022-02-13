28019ee131d238d15a10070e9aa289d0940ad8f5
steve ulrich <sulrich@botwerks.org>
2022-02-13 09:42:09 -0600

    misc. updates to shell config

--- zsh-custom/hosts/krusty.zsh
+++ zsh-custom/hosts/krusty.zsh
@@ -1,20 +1,6 @@
 # arista specific functions for interacting w/internal services
 fpath=(/usr/local/share/zsh-completions $fpath)
 
-# mail rollover tools - tis moves my mailbox to the right spot
-rolloutbox () {
-  ARCH_YEAR=$(date -v -2m +%Y)
-  ARCH_BASE=${HOME}/Documents/archives/mail/${ARCH_YEAR}/outbox
-  ${HOME}/bin/split-maildirs.pl --keep_recent --arch_dir=$ARCH_BASE -src_dir=${HOME}/mail outbox
-}
-
-# kill offlineimap instances and clean up the lock file. 
-koff () {
-  pkill -if offlineimap
-  rm "${HOME}/.offlineimap/arista.lock"
-  offlineimap -u basic
-}
-
 case-blurb() {
   local CASEBLURB="${HOME}/.home/templates/text/case-blurb.txt"
   if [ "$1" = "" ]
@@ -31,6 +17,16 @@ EOFUSAGE
   fi
 }
 
+function clab-start() {
+  docker run --rm -it --privileged                 \
+      --network host                               \
+      -v /var/run/docker.sock:/var/run/docker.sock \
+      -v /var/run/netns:/run/netns                 \
+      --pid="host"                                 \
+      -w $CLAB_DIR                                 \
+      -v $CLAB_DIR:$CLAB_DIR                       \
+      ghcr.io/srl-labs/clab bash
+}
 # misc. arista specific aliases
 alias ago="TERM=vt100 ssh -l admin -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 source "${HOME}/.home/pb.sh"
--- zsh-custom/platform/Darwin.zsh
+++ zsh-custom/platform/Darwin.zsh
@@ -9,12 +9,12 @@ alias lsusb='system_profiler SPUSBDataType'
 alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
 alias topc="top -o cpu"
 alias vctl="/Applications/VMware\ Fusion.app/Contents/Public/vctl"
+alias loc="less ${OC_PATH_DUMP}"
+alias lopr="less ${ANET_PATHREPORT}"
 
 function set_volume() { sudo osascript -e "set Volume $1" }
 function ql () { qlmanage -p "$@" >& /dev/null & }
 function iterm-title () { echo -ne "\033]0;"${1}"\007" }
-
-
 
 vterm_printf(){
   if [ -n "$TMUX" ]; then
@@ -29,15 +29,29 @@ vterm_printf(){
   fi
 }
 
-
 # sometimes we want to install something using the homebrew version of python.
 # this is expecially useful when dealing with things that happen to be linked
 # against the brew python installs and there's a pip module we need.
-pyenv-brew-relink() {
+function pyenv-brew-relink() {
   rm -f "${HOME}/.pyenv/versions/*-brew"
 
   for i in $(brew --cellar python)/*; do
     echo "brew version: $i"
     ln -s "${i}" "${HOME}/.pyenv/versions/${i##/*/}-brew"
   done
+}
+
+# legacy imap/mutt mail handling tools
+# mail rollover tools - tis moves my mailbox to the right spot
+rolloutbox () {
+  ARCH_YEAR=$(date -v -2m +%Y)
+  ARCH_BASE=${HOME}/Documents/archives/mail/${ARCH_YEAR}/outbox
+  ${HOME}/bin/split-maildirs.pl --keep_recent --arch_dir=$ARCH_BASE -src_dir=${HOME}/mail outbox
+}
+
+# kill offlineimap instances and clean up the lock file. 
+koff () {
+  pkill -if offlineimap
+  rm "${HOME}/.offlineimap/arista.lock"
+  offlineimap -u basic
 }
