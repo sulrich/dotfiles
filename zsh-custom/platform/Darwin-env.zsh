16c92284ae7ffa67f05c2cb1ebea43239acca994
steve ulrich <sulrich@botwerks.org>
2022-05-08 08:42:43 -0500

    removed old day1 environment var

--- zsh-custom/platform/Darwin-env.zsh
+++ zsh-custom/platform/Darwin-env.zsh
@@ -8,9 +8,6 @@ export VAGRANT_DEFAULT_PROVIDER="virtualbox"
 #export VAGRANT_DEFAULT_PROVIDER="vmware_fusion"
 #export VAGRANT_VMWARE_CLONE_DIRECTORY="/Volumes/JetDrive/vagrant-clone"
 
-# day1 journal location - this seems like voodoo
-export D1J="${HOME}/Library/Group Containers/5U8NS4GX82.dayoneapp2/Data/Auto Import/Default Journal.dayone"
-
 # convenience environment variable to get to the iCloud root directory
 export ICLOUD="${HOME}/Library/Mobile Documents/com~apple~CloudDocs"
 
