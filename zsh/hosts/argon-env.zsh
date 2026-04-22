ebeebeb03e000f0d4266042342885b1d9e313c13
steve ulrich <sulrich@botwerks.org>
2026-04-22 10:56:14 -0500

    import API creds

--- zsh/hosts/argon-env.zsh
+++ zsh/hosts/argon-env.zsh
@@ -1,3 +1,5 @@
 #---------------------------------------------------------------------
 # ansible vars
 export ANSIBLE_INVENTORY="${HOME}/src/ansible/etc/hosts"
+
+source "$HOME/.credentials/00-api-creds.env"
